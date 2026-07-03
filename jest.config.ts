9a0d697c8eced8caa169fb35cd77e6987cdb4f86
kfarkasHU <sohamar@farkask.eu>
2026-07-03 21:55:56 +0200

    feature: coverage generation
    Closes #13

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,10 @@ import type { Config } from 'jest';
 const config: Config = {
   testEnvironment: 'node',
   testMatch: ['<rootDir>/generated/jest/**/*.spec.ts'],
+  collectCoverageFrom: ['<rootDir>/test/**/*.ts'],
+  coverageDirectory: '<rootDir>/coverage/jest',
+  coverageProvider: 'v8',
+  coverageReporters: ['html', 'text-summary', 'json-summary'],
   transform: {
     '^.+\\.ts$': ['ts-jest', { tsconfig: 'tsconfig.jest.json' }],
   },
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,12 @@ export default defineConfig({
   test: {
     include: ['generated/vitest/**/*.spec.ts'],
     environment: 'node',
+    coverage: {
+      provider: 'v8',
+      include: ['test/**/*.ts'],
+      exclude: ['generated/**/*.spec.ts'],
+      reportsDirectory: 'coverage/vitest',
+      reporter: ['html', 'text-summary', 'json-summary'],
+    },
   },
 });
